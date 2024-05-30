52e8b0e297bf3eb23e3a8cea14e406da9dec5279
Andres Felipe Arevalo Suarez <36401985+andresfelipear@users.noreply.github.com>
2024-05-30 16:34:30 -0700

    fixing the styling

--- src/code/Employees.java
+++ src/code/Employees.java
@@ -8,6 +8,7 @@ import java.util.*;
  */
 public class Employees
 {
+    private final List<Employee> employees;
     private final List<HockeyPlayer> hockeyPlayers;
     private final List<Professor> professors;
     private final List<Parent> parents;
@@ -19,7 +20,6 @@ public class Employees
     {
         Employees employees;
         employees = new Employees();
-
     }
 
     /**
@@ -28,39 +28,109 @@ public class Employees
      */
     public Employees()
     {
+        final Employee emp1;
+        final Employee emp2;
+        final Employee emp3;
+        final Employee emp4;
+        final Employee emp5;
+        final Employee emp6;
+        final Employee emp7;
+        final Employee emp8;
+        final Employee emp9;
+        final Employee emp10;
+        final Employee emp11;
+        final Employee emp12;
+        final Employee emp13;
+        final Employee emp14;
+        final Employee emp15;
+        final Employee emp16;
+        final Employee emp17;
+        final Employee emp18;
+        final Employee emp19;
+        final Employee emp20;
+
+        employees = new ArrayList<>();
         hockeyPlayers = new ArrayList<>();
         professors = new ArrayList<>();
         parents = new ArrayList<>();
         gasAttendants = new ArrayList<>();
 
-        // Hockey Players
-        hockeyPlayers.add(new HockeyPlayer("Wayne Gretzky",894));
-        hockeyPlayers.add(new HockeyPlayer("Who Ever",0));
-        hockeyPlayers.add(new HockeyPlayer("Brent Gretzky",1));
-        hockeyPlayers.add(new HockeyPlayer("Pavel Bure",437));
-        hockeyPlayers.add(new HockeyPlayer("Jason Wilder",0));
+        emp1 = new HockeyPlayer("Wayne Gretzky", 894);
+        emp2 = new HockeyPlayer("Who Ever", 0);
+        emp3 = new HockeyPlayer("Brent Gretzky", 1);
+        emp4 = new HockeyPlayer("Pavel Bure", 437);
+        emp5 = new HockeyPlayer("Jason Wilder", 0);
 
-        // Professors
-        professors.add(new Professor("Albert Einstein", "Physics"));
-        professors.add(new Professor("Jason Wilder", "Computer Systems"));
-        professors.add(new Professor("Richard Feynman", "Physics"));
-        professors.add(new Professor("BCIT Instructor", "Computer Systems"));
-        professors.add(new Professor("Kurt Godel", "Logic"));
+        emp6 = new Professor("Albert Einstein", "Physics");
+        emp7 = new Professor("Jason Wilder", "Computer Systems");
+        emp8 = new Professor("Richard Feynman", "Physics");
+        emp9 = new Professor("BCIT Instructor", "Computer Systems");
+        emp10 = new Professor("Kurt Godel", "Logic");
 
-        // Parent
-        parents.add(new Parent("Tiger Woods", 1));
-        parents.add(new Parent("Super Mom", 168));
-        parents.add(new Parent("Lazy Larry", 20));
-        parents.add(new Parent("Ex Hausted", 168));
-        parents.add(new Parent("Super Dad", 167));
+        emp11 = new Parent("Tiger Woods", 1);
+        emp12 = new Parent("Super Mom", 168);
+        emp13 = new Parent("Lazy Larry", 20);
+        emp14 = new Parent("Ex Hausted", 168);
+        emp15 = new Parent("Super Dad", 167);
 
-        // Gas Attendant
-        gasAttendants.add(new GasStationAttendant("Joe Smith", 10));
-        gasAttendants.add(new GasStationAttendant("Tony Baloney", 100));
-        gasAttendants.add(new GasStationAttendant("Benjamin Franklin", 100));
-        gasAttendants.add(new GasStationAttendant("Mary Fairy", 101));
-        gasAttendants.add(new GasStationAttendant("Bee See", 1));
+        emp16 = new GasStationAttendant("Joe Smith", 10);
+        emp17 = new GasStationAttendant("Tony Baloney", 100);
+        emp18 = new GasStationAttendant("Benjamin Franklin", 100);
+        emp19 = new GasStationAttendant("Mary Fairy", 101);
+        emp20 = new GasStationAttendant("Bee See", 1);
 
+        employees.add(emp1);
+        employees.add(emp2);
+        employees.add(emp3);
+        employees.add(emp4);
+        employees.add(emp5);
+        employees.add(emp6);
+        employees.add(emp7);
+        employees.add(emp8);
+        employees.add(emp9);
+        employees.add(emp10);
+        employees.add(emp11);
+        employees.add(emp12);
+        employees.add(emp13);
+        employees.add(emp14);
+        employees.add(emp15);
+        employees.add(emp16);
+        employees.add(emp17);
+        employees.add(emp18);
+        employees.add(emp19);
+        employees.add(emp20);
+
+        // Populate the separate lists
+        for(Employee e : employees)
+        {
+            if(e instanceof HockeyPlayer)
+            {
+                hockeyPlayers.add((HockeyPlayer) e);
+            }
+            else if(e instanceof Professor)
+            {
+                professors.add((Professor) e);
+            }
+            else if(e instanceof Parent)
+            {
+                parents.add((Parent) e);
+            }
+            else if(e instanceof GasStationAttendant)
+            {
+                gasAttendants.add((GasStationAttendant) e);
+            }
+        }
+
+        printDetails();
+    }
+
+    /**
+     * Prints each list before and after it was sort.
+     * After it prints all the elements of each list that are equals.
+     *
+     */
+    private void printDetails()
+    {
         System.out.println("Hockey Players - Before");
         System.out.println(hockeyPlayers);
         System.out.println("Hockey Players - After");
